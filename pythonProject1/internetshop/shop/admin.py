1365fbb8d8525b4f3b6b464f40753dc33c7ea2be
Go <thewdncom@gmail.com>
2024-07-02 07:25:58 +0300

    добавлена админка

--- pythonProject1/internetshop/shop/admin.py
+++ pythonProject1/internetshop/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
