db2ab84502d724b80170bc612f8f98145ffe203b
Go <thewdncom@gmail.com>
2024-07-06 14:01:13 +0300

    добавлены характеристики

--- pythonProject1/internetshop/shop/models.py
+++ pythonProject1/internetshop/shop/models.py
@@ -7,5 +7,7 @@ class Product(models.Model):
     price = models.IntegerField()
     image_url = models.CharField(max_length=1024)
 
+    speed = models.IntegerField(null=True, blank=True)
+    ram = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.name
