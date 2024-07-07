eccbb829e55be0ccaa2a8b60408621dd72b8d55c
Go <thewdncom@gmail.com>
2024-07-07 05:55:09 +0300

    правки в models.py, требовался отступ

--- pythonProject1/internetshop/shop/models.py
+++ pythonProject1/internetshop/shop/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
         return self.name
 
 
-    class Review(models.Model):
+class Review(models.Model):
         product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
         author = models.CharField(max_length=256)
